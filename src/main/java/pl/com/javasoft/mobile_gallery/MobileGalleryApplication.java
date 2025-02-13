9858367da54392c7825fadd81d6f3d688ae4195b
Krzysztof Pobozan <krzpob@gmail.com>
2025-02-13 20:48:24 +0100

    init spring-boot

---
+++ src/main/java/pl/com/javasoft/mobile_gallery/MobileGalleryApplication.java
@@ -0,0 +1,13 @@
+package pl.com.javasoft.mobile_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/javasoft/mobile_gallery/MobileGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.javasoft.mobile_gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
